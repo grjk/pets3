f56d79651217e5c8e90176601518dcbd2e664754
Joshua Kristiansen <jkristiansen@mail.greenriver.edu>
2020-01-31 13:23:55 -0500

    Changed index.php and method of posting in views

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ require("vendor/autoload.php");
 
 // Instantiate Fat-Free
 $f3 = Base::Instance();
+$f3->set('colors', array('pink', 'green', 'blue'));
 
 // Define a default route (view)
 $f3 -> route("GET /", function () {
@@ -26,14 +27,14 @@ $f3 -> route("GET /order", function () {
 });
 
 // Define a default route (view)
-$f3 -> route("POST /order2", function () {
+$f3 -> route("GET|POST /order2", function () {
     $view = new Template();
     $_SESSION['animal'] = $_POST['animal'];
     echo $view->render("views/form2.html");
 });
 
 
-$f3 -> route("POST /results", function () {
+$f3 -> route("GET|POST /results", function () {
     $view = new Template();
     $_SESSION['color'] = $_POST['color'];
     echo $view->render("views/results.html");
